8f717033d4e008ef0f74236ee6f036ba27750cc7
GoodbyeMyself <15500409521@163.com>
2025-05-12 17:18:36 +0800

    Docker 部署 Next   配置文件

--- Next.js/next-test/next.config.ts
+++ Next.js/next-test/next.config.ts
@@ -6,7 +6,9 @@ const nextConfig: NextConfig = {
     // 可选：确保静态资源（如 _next/static）路径正确
     assetPrefix: '/next-test/', 
     // 可选：确保路径以斜杠结尾，防止重定向问题
-    trailingSlash: true
+    trailingSlash: true,
+    // Docker 部署
+    output: 'standalone'
 };
 
 export default nextConfig;
